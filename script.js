27a653b1088f09884e48c5a62f1cc4373fd72d13
Tomtom141 <shegotomtom@outlook.com>
2026-01-23 05:51:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function scrollToContact(){ document.getElementById('contact').scrollIntoView({behavior:'smooth'}); }
+function sendMessage(e){ e.preventDefault(); alert('Message sent successfully!'); }
